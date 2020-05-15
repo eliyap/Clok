216d69d3d4768bfdaeead22a0fec492e589f7839
Elijah <emlyap99@gmail.com>
2020-05-15 15:45:07 -0700

    Create EntryList.swift

---
+++ Details/EntryList.swift
@@ -0,0 +1,21 @@
+//
+//  EntryList.swift
+//  Trickl
+//
+//  Created by Secret Asian Man 3 on 20.05.15.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import SwiftUI
+
+struct EntryList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EntryList_Previews: PreviewProvider {
+    static var previews: some View {
+        EntryList()
+    }
+}
