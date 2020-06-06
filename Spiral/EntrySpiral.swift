0b70b5bc8a90e82ee630fc5ebe754bc95c27eb94
Elijah Yap <yap@usc.edu>
2020-06-06 13:16:01 -0700

    Update EntrySpiral.swift
    temporarily drop stroke animation

--- Spiral/EntrySpiral.swift
+++ Spiral/EntrySpiral.swift
@@ -11,17 +11,13 @@ import SwiftUI
 struct EntrySpiral: View {
     @ObservedObject var entry:TimeEntry = TimeEntry()
     @EnvironmentObject var listRow: ListRow
-    @State private var stroke:CGFloat = stroke_weight
+//    @State private var stroke:CGFloat = stroke_weight
     @State private var opacity:Double = 1
     
     var body: some View {
                 
         Spiral(theta1: entry.startTheta, theta2: entry.endTheta)
-            .stroke(entry.project_hex_color, style: StrokeStyle(
-            lineWidth: self.stroke,
-            lineCap: .butt
-            )
-        )
+            .fill(entry.project_hex_color)
             
         .gesture(TapGesture().onEnded(){_ in
             /// pass selection to global variable
@@ -34,13 +30,13 @@ struct EntrySpiral: View {
             /// brief bounce animation
             /// per Zero Punctuation advice, peak quickly then drop off slowly
             withAnimation(.linear(duration: 0.1)){
-                self.stroke -= 2
+//                self.stroke += 2
                 // drop the opacity to take on more BG color
                 self.opacity -= 0.25
             }
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                 withAnimation(.linear(duration: 0.3)){
-                    self.stroke = stroke_weight
+//                    self.stroke = stroke_weight
                     self.opacity = 1
                 }
             }
