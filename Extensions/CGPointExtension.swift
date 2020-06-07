0478992db05d954f0e66da81766323b1f457f451
Elijah Yap <yap@usc.edu>
2020-06-07 14:23:16 -0700

    simplify methods

--- Extensions/CGPointExtension.swift
+++ Extensions/CGPointExtension.swift
@@ -10,7 +10,25 @@ import Foundation
 import SwiftUI
 
 extension CGPoint {
+    /**
+     magnitude of distance from the origin
+     */
     func magnitude() -> CGFloat {
         (x*x + y*y).squareRoot()
+    }
+    
+    /**
+     angle of line from origin to point, off of the +x axis
+     +ve is counter clockwise
+     normalized to a [0, 360] degree range
+     */
+    func angle() -> Angle {
+        /// take the arctan, with coordinate zero at circle's center
+        var angle = Double(atan(y/x))
+        
+        angle += (y > 0 && x > 0) ? 0 : /// angle is in 1st quadrant, add nothing
+            (x < 0) ? Double.pi : /// 2nd & 3rd quadrant, add pi
+            2 * Double.pi /// 4th quardrant, add 2 pi
+        return Angle(radians: angle)
     }
 }
