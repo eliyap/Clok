5dc2c70a596762a4d8ac7a3403071103c863254e
Elijah Yap <yap@usc.edu>
2020-06-07 15:13:52 -0700

    Revert "simplify methods"
    This reverts commit 0478992db05d954f0e66da81766323b1f457f451.

--- Knob/KnobView.swift
+++ Knob/KnobView.swift
@@ -19,17 +19,45 @@ struct KnobAngle {
     private let threshold = Angle(degrees: 270)
     
     mutating func update(geo: GeometryProxy, value: DragGesture.Value) -> Void {
+        let pos = localPosition(geo: geo, value: value)
+        
         /// get angle
-        lead = CGPoint(x:
-            value.location.x - geo.size.width / 2,
-                y: geo.size.width / 2 - value.location.y
-        ).angle()
+        lead = getDegrees(pos: pos)
         
         /// Lead - Lag comparison allows us to detect rollover (i.e. complete rotations)
         /// if angle dramatically increases, deduct 1 clockwise turn
         if lead - lag > threshold { rotations -= 1 }
         /// if angle dramatically decreases, add 1 clockwise turn
         if lead - lag < -threshold { rotations += 1 }
+    }
+    
+    /// get the cursor position relative to circle's center
+    func localPosition(geo: GeometryProxy, value: DragGesture.Value) -> CGPoint {
+        /// +y being up and +x being rightwards,
+        CGPoint(x:
+            value.location.x - geo.size.width / 2,
+                y: geo.size.width / 2 - value.location.y
+        )
+    }
+    
+    /// finds the angle of the tap gesture
+    /// with 0 at +x axis, and +ve being CCW
+    func getDegrees(pos: CGPoint) -> Angle {
+        let y = pos.y
+        let x = pos.x
+        
+        /// take the arctan, with coordinate zero at circle's center
+        var angle = Double(atan(y/x))
+        
+        angle += (y > 0 && x > 0) ?
+            /// angle is in 1st quadrant, add nothing
+            0 :
+            /// 2nd & 3rd quadrant, add pi
+            (x < 0) ?
+                Double.pi :
+            /// 4th quardrant, add 2 pi
+            2 * Double.pi
+        return Angle(radians: angle)
     }
     
     mutating func updateFrame(_ frame:WeekTimeFrame) -> WeekTimeFrame {
