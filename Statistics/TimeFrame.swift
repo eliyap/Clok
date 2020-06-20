c798fa8cfce5dd8595bea3192b083b3678b0c596
Elijah Yap <yap@usc.edu>
2020-06-20 15:38:32 -0700

    Update TimeFrame.swift

--- Statistics/TimeFrame.swift
+++ Statistics/TimeFrame.swift
@@ -35,59 +35,90 @@ struct WeekTimeFrame {
     /**
      we expect time entries to already be filtered down between some date range
      */
-    init (zero:Date, entries entries_:[TimeEntry]) {
+    init (zero: Date, entries entries_: [TimeEntry]) {
         frame = TimeFrame(start: zero, end: zero + weekLength)
         entries = entries_
         df.timeStyle = .short
     }
     
     /**
-     a smart measure of what time each day you start an activity
-     based on "modified midnight" metric
+     find the modified midnight: the average of all start and end times,
+     +12hr to place it on the opposite side of the clock
+     designed to be placed OPPOSITE most of the provided time entries, thus dividing them into sensible "days"
      */
-    func avgStartTime(_ terms:SearchTerm) -> Date {
+    func modifiedMidnight(for entries: [TimeEntry]) -> Date {
+        [
+            entries.map{ $0.start }.meanTime(),
+            entries.map{ $0.end   }.meanTime()
+        ].meanTime() + (dayLength / 2)
+    }
+    
+    /**
+     find the most sensible way to divide these time entries into "days"
+     with each day spanning at most 24 hours, and starting / ending at some modified midnight
+    */
+    func modifiedDays(for entries: [TimeEntry]) -> [DayTimeFrame] {
+        let modMN = modifiedMidnight(for: entries)
+        /// find day time frames, using modified midnight
+        return daySlices(
+            start: roundDown(frame.start, to: modMN),
+            end: frame.end
+        )
+    }
+    
+    /**
+     a smart measure of what time each day you start an activity
+     */
+    func avgStartTime(for terms: SearchTerm) -> Date {
         /// filter and sort in chronological order by start
         var e = entries.matching(terms)
         e.sort(by: {$0.start < $1.start})
-        
-        /**
-         modified midnight is the average of all start and end times,
-         +12hr to place it on the opposite side of the clock
-         */
-        let modMN = [
-            e.map{ $0.start }.meanTime(),
-            e.map{ $0.end   }.meanTime()
-        ].meanTime() + (dayLength / 2)
+        var days = modifiedDays(for: e)
         
         /// list of the time entries that were the first started of their day
         var firstStarts = [TimeEntry]()
         
-        /// find day time frames, using modified midnight
-        var days = daySlices(
-            start: roundDown(frame.start, to: modMN),
-            end: frame.end
-        )
-        
-        /// iterate over list, only adding the *first started* entry in a day frame that hasn't yet been filled
+        /// iterate over list, adding the entry that *first started* that Day frame
         e.forEach { entry in
             for idx in 0..<days.count {
                 if days[idx].frame.containsStartOf(entry) {
-                    /// add the *first started* entry in a day frame
                     firstStarts.append(entry)
-        
-                    /// pop this day, so no other entries starting on this day can be added
                     days.remove(at: idx)
-                    /// move on to next entry
+                    break
+                }
+            }
+        }
+        return firstStarts
+            .map{$0.start}
+            .meanTime()
+    }
+    
+    /**
+     a smart measure of what time each day you end an activity
+     */
+    func avgEndTime(for terms: SearchTerm) -> Date {
+        /// filter and sort in reverse chronological order by end
+        var e = entries.matching(terms)
+        e.sort(by: {$0.end > $1.end})
+        var days = modifiedDays(for: e)
+        
+        /// list of the time entries that were the first ended of their day
+        var lastEnds = [TimeEntry]()
+        
+        /// iterate over list backwards, adding the entry that *last ended* that Day frame
+        e.forEach { entry in
+            for idx in stride(from: days.count - 1, through: 0, by: -1) {
+                if days[idx].frame.containsEndOf(entry) {
+                    lastEnds.append(entry)
+                    days.remove(at: idx)
                     break
                 }
             }
         }
         
-        let avg = firstStarts
-            .map{$0.start}
+        return lastEnds
+            .map{$0.end}
             .meanTime()
-        
-        return avg
     }
 }
 
