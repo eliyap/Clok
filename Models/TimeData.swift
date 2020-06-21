c3ae9d0d15ff1680b6c458ac0bb95e4e07e3a27c
Elijah Yap <yap@usc.edu>
2020-06-20 20:17:40 -0700

    Create TimeData.swift
    changes report to a environment obj

---
+++ Models/TimeData.swift
@@ -0,0 +1,13 @@
+//
+//  Data.swift
+//  Trickl
+//
+//  Created by Secret Asian Man 3 on 20.06.20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import Foundation
+
+final class TimeData: ObservableObject {
+    @Published var report = Report()
+}
