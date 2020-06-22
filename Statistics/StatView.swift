5c1c7b680822bc1de0a3b7656a5739afe80d2ce5
Elijah Yap <yap@usc.edu>
2020-06-21 17:04:26 -0700

    add description control

--- Statistics/StatView.swift
+++ Statistics/StatView.swift
@@ -12,37 +12,39 @@ struct StatView: View {
     @EnvironmentObject var data: TimeData
     @EnvironmentObject var zero: ZeroDate
     @State private var project = Project.any
+    @State private var descriptionField: String = ""
+    @State private var description: String = ""
     
     var body: some View {
         NavigationView {
             VStack {
                 List{
-                    NavigationLink(destination: ProjectListView(
-                        chosen: self.$project,
-                        projects: self.data.projects().sorted()
-                    )
-                        .navigationBarTitle("Projects", displayMode: .inline)
-                        .navigationBarHidden(false)
-                    ) {
-                        HStack{
-                            Rectangle()
-                                .frame(width: listLineInset)
-                                .foregroundColor(project.color)
-                            Text(project.name)
-                                .bold()
-                        }
+                    ProjectListLink()
+                    HStack {
+                        Rectangle()
+                            .frame(width: listLineInset)
+                            .foregroundColor(project.color)
+                        TextField(
+                            "No Description",
+                            text: self.$descriptionField,
+                            onCommit: {
+                                /// only change description when user presses return
+                                self.description = self.descriptionField
+                            }
+                        )
+                        Text(self.description)
+                        
                     }
                     .modifier(fillRow())
                 }
                 .modifier(roundedList())
                 
-                
                 StatDisplayView(for: WeekTimeFrame(
                     start: self.zero.date,
                     entries: self.data.report.entries,
                     terms: SearchTerm(
-                        project: .noProject,
-                        description: "",
+                        project: self.project,
+                        description: self.description,
                         byProject: true,
                         byDescription: false
                     )
@@ -52,5 +54,27 @@ struct StatView: View {
             }
         }
         .navigationViewStyle(StackNavigationViewStyle())
+    }
+    
+    /**
+     allows user to choose a different project
+     */
+    func ProjectListLink() -> some View {
+        NavigationLink(destination: ProjectListView(
+            chosen: self.$project,
+            projects: self.data.projects().sorted()
+        )
+            .navigationBarTitle("Projects", displayMode: .inline)
+            .navigationBarHidden(false)
+        ) {
+            HStack{
+                Rectangle()
+                    .frame(width: listLineInset)
+                    .foregroundColor(project.color)
+                Text(project.name)
+                    .bold()
+            }
+        }
+        .modifier(fillRow())
     }
 }
