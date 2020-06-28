1fd60b39d42a16db4da35855de6e0eeec0621fdb
Ipsky <yap@usc.edu>
2020-06-28 12:21:20 -0700

    break data loading out into an extension

--- Clok/ContentView.swift
+++ Clok/ContentView.swift
@@ -37,50 +37,11 @@ struct ContentView: View {
         }
         .onAppear {
             /// load data immediately
-//                self.loadData()
+                self.loadData()
             
             let saveSuccessful: Bool = KeychainWrapper.standard.set("thisisatestkey", forKey: "TogglAPIKey")
             let retrievedString: String? = KeychainWrapper.standard.string(forKey: "TogglAPIKey")
             print(retrievedString)
         }
     }
-    
-    func loadData() {
-            DispatchQueue.global(qos: .utility).async {
-                
-                // assemble request URL (page is added later)
-                let df = DateFormatter()
-                df.dateFormat = "yyyy-MM-dd" // ISO 8601 format, day precision
-                
-                let since = df.string(from: Date().addingTimeInterval(TimeInterval(-86400 * 365))) // grab 365 days...
-                let until = df.string(from: Date())                                                // ...from today
-                
-                // documented at https://github.com/toggl/toggl_api_docs/blob/master/reports.md
-                let base_url = "https://toggl.com/reports/api/v2/"
-                let api_string = "\(base_url)details?" + [
-                    "user_agent=\(user_agent)",    // identifies my app
-                    "workspace_id=\(myWorkspace)", // provided by the User
-                    "since=\(since)",
-                    "until=\(until)"
-                    ].joined(separator: "&")
-                
-                let result = toggl_request(api_string: api_string, token: myToken)
-                
-                DispatchQueue.main.async {
-                    switch result {
-                    case let .success(report):
-                        /// hand back the complete report
-                        self.data.report = report
-                        /// and remove the loading screen
-                        self.loaded = true
-                        
-                    case .failure(.request):
-                        // temporary micro-copy
-                        print("We weren't able to fetch your data. Maybe the internet is down?")
-                    case let .failure(error):
-                        print(error)
-                    }
-                }
-            }
-        }
 }
