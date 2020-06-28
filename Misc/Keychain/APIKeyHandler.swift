b33d4b775bb3cf01c642ba9fef385d342b09230b
Ipsky <yap@usc.edu>
2020-06-28 13:21:15 -0700

    drop preview

--- Misc/Keychain/APIKeyHandler.swift
+++ Misc/Keychain/APIKeyHandler.swift
@@ -13,9 +13,3 @@ struct APIKeyHandler: View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
-
-struct APIKeyHandler_Previews: PreviewProvider {
-    static var previews: some View {
-        APIKeyHandler()
-    }
-}
