7b15f4096ed3d4e3c53827037ba76daf0ff4002e
Ipsky <yap@usc.edu>
2020-07-03 16:42:05 -0700

    Create Fullscreen.swift
    makes a view hog the whole screen, like what the loading screen and login views should do

---
+++ Misc/Fullscreen.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 3/7/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct FullscreenModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(UIColor.systemBackground))
+            .edgesIgnoringSafeArea(.all)
+            .transition(.opacity)
+    }
+}
