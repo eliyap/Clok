7f24897f1207a10e6f1004a0bd50f041639098a5
Ipsky <yap@usc.edu>
2020-07-02 19:35:53 -0700

    entry list
    add headers

--- EntryList/EntryList.swift
+++ EntryList/EntryList.swift
@@ -13,12 +13,62 @@ struct EntryList: View {
     @EnvironmentObject var data: TimeData
     @EnvironmentObject var zero: ZeroDate
     
+    private let df = DateFormatter()
+    
+    init() {
+        df.setLocalizedDateFormatFromTemplate("MMMdd")
+    }
+    
     var body: some View {
         List {
-            ForEach(data.report.entries.within(interval: weekLength, of: zero.date), id: \.id) { entry in
-                Text(entry.description)
+            ForEach(Days(), id: \.id) { day in
+                Section(header: Text(df.string(from: day.start))) {
+                    ForEach(day.entries, id: \.id) { entry in
+                        Text(entry.description)
+                    }
+                }
             }
         }
+    }
+    
+    struct Day {
+        var id: Int
+        var start: Date
+        var entries: [TimeEntry]
+    }
+    
+    func Days() -> [Day] {
+        /// restrict to current week
+        let entries = data.report.entries.within(interval: weekLength, of: zero.date)
+        
+        var days = [Day]()
+        let cal = Calendar.current
+        for mn in stride(
+            from: cal.startOfDay(for: zero.date),
+            through: cal.startOfDay(for: zero.date + weekLength),
+            by: dayLength
+        ) {
+            days.append(Day(
+                id: Int(mn.timeIntervalSince1970),
+                start: mn,
+                entries: entries.within(interval: dayLength, of: mn)
+            ))
+        }
+        return days
+    }
+    
+    func HeaderFor(section: Day) -> String {
+        let cal = Calendar.current
+        let currentYear = cal.component(.year, from: Date())
+        let zeroYear = cal.component(.year, from: zero.date)
+        
+        /// day of week, day of month, MMM
+        return [
+            section.start.shortWeekday(),
+            df.string(from: section.start),
+            /// plus optional YYYY if it is not current year
+            ((currentYear == zeroYear) ? "" : "\(zeroYear)")
+        ].joined(separator: " ")
     }
 }
 
---
+++ EntryList/EntryView.swift
@@ -0,0 +1,18 @@
+//
+//  EntryView.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 2/7/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import SwiftUI
+
+struct EntryView: View {
+    var entry: TimeEntry
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
