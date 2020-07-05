734d5f5239943a66dae36b811021962ce5405455
Ipsky <yap@usc.edu>
2020-07-04 18:42:59 -0700

    add URL request to widget

--- ClokWidget/ClokWidget.swift
+++ ClokWidget/ClokWidget.swift
@@ -13,16 +13,31 @@ import Intents
 
 struct Provider: IntentTimelineProvider {
     public func snapshot(for configuration: ConfigurationIntent, with context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), data: 100)
+        let entry = SimpleEntry(date: Date(), data: "100")
         completion(entry)
     }
 
     public func timeline(for configuration: ConfigurationIntent, with context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = [
-            SimpleEntry(date: Date(), data: 0),
-            SimpleEntry(date: Date() + 1.0, data: 1),
-            SimpleEntry(date: Date() + 5.0, data: 2)
-        ]
+        loadUserData { (user, error) in
+            guard let user = user else {
+                var entries: [SimpleEntry] = [
+                    SimpleEntry(date: Date(), data: "0"),
+                    SimpleEntry(date: Date() + 1.0, data: "1"),
+                    SimpleEntry(date: Date() + 5.0, data: "2")
+                ]
+                let timeline = Timeline(entries: entries, policy: .atEnd)
+                completion(timeline)
+                return
+            }
+            
+            let timeline = Timeline(
+                entries: [SimpleEntry(date: Date(), data: user.email)],
+                policy: .atEnd
+            )
+            completion(timeline)
+            return
+        }
+        
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
@@ -32,14 +47,13 @@ struct Provider: IntentTimelineProvider {
 //            entries.append(entry)
 //        }
 
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
+        
     }
 }
 
 struct SimpleEntry: TimelineEntry {
     public let date: Date
-    public let data: Int
+    public let data: String
 }
 
 struct PlaceholderView : View {
@@ -63,11 +77,14 @@ struct ClokWidget: Widget {
     private let kind: String = "ClokWidget"
 
     public var body: some WidgetConfiguration {
-        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider(), placeholder: PlaceholderView(entry: SimpleEntry(date: Date(), data: -1))) { entry in
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider(), placeholder: PlaceholderView(entry: SimpleEntry(date: Date(), data: "-1"))) { entry in
             ClokWidgetEntryView(entry: entry)
         }
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
         .supportedFamilies([.systemSmall])
+        .onBackgroundURLSessionEvents { (sessionID, completion) in
+            
+        }
     }
 }
