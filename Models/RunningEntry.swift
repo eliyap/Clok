1e8a6df053322263d2b0e518a8733b0cb19d271b
Ipsky <yap@usc.edu>
2020-07-05 08:22:09 -0700

    Update RunningEntry.swift

--- Models/RunningEntry.swift
+++ Models/RunningEntry.swift
@@ -10,31 +10,17 @@ import Foundation
 import SwiftUI
 
 struct RunningEntry {
+
     let id:Int
-    
     let start: Date // needs to be coerced from ISO 8601 date / time format (YYYY - MM - DDTHH: MM: SS)
-    
-    // categorization parameters
     let project: Project
-    let tid: Int?
-    let task: String?
     let description: String // not nullable
+    let df = DateFormatter()
     
-    // used to create a TimeEntry from data parsed from JSON
-    init?(
-        _ data:Dictionary<String,AnyObject>
-    ){
+    // parse from JSON
+    init?(from data: [String : AnyObject], project: Project){
         // initialize DateFormatter to handle ISO8601 strings
-        let df = DateFormatter()
         df.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
-        
-        // nullable properties
-        // downcast cannot be checked via "guard"
-        let pid = data["pid"] as? Int
-        let project = data["project"] as? String
-        let project_hex_color = data["project_hex_color"] as? String
-        let tid = data["tid"] as? Int
-        let task = data["task"] as? String
         
         // check that these properties are not nil
         guard
@@ -44,28 +30,13 @@ struct RunningEntry {
             // get Dates from ISO8601 string
             data["start"] != nil,
             let startString = data["start"] as? String,
-            let start = df.date(from: startString),
-            data["end"] != nil,
-            let endString = data["end"] as? String,
-            let end = df.date(from: endString)
+            let start = df.date(from: startString)
         else { return nil }
-        // sanity check, make sure end time is after start time
-        guard start < end else { return nil }
         
         self.id = id
-        if let name = project, let id = pid {
-            self.project = Project(
-                name: name,
-                color: Color(hex: project_hex_color!),
-                id: id
-            )
-        } else {
-            self.project = .noProject
-        }
-        self.tid = tid
-        self.task = task
         self.description = description
         self.start = start
+        self.project = project
     }
     
     /// Headlining description,
