e890d729e21262e0c089b03544fbd7f04f94ae20
Ipsky <yap@usc.edu>
2020-07-30 16:01:07 -0700

    Update LineBar.swift

--- LineGraph/LineBar.swift
+++ LineGraph/LineBar.swift
@@ -19,7 +19,8 @@ struct LineBar: View {
     @State private var offset = CGFloat.zero
     
     private var size: CGSize
-    private let cornerScale = CGFloat(1.0/8.0)
+    private let cornerScale = CGFloat(1.0/18.0)
+    
     /// determines what proportion of available horizontal space to consume
     static let thicc = CGFloat(0.8)
     var bound: Bound
