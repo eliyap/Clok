1a3423cf55d4d7a1fbf2faf17ee5c262f553c285
Ipsky <yap@usc.edu>
2020-08-06 12:10:19 -0700

    comment

--- Misc/Divisions.swift
+++ Misc/Divisions.swift
@@ -10,7 +10,6 @@ import SwiftUI
 fileprivate let minSize = 35 /// allocate at least this amount of space per division
 
 /// ways to divide 24 hours into equal portions
-/// choose whichever gives at least some amount of space to each division
 func evenDivisions(for height: CGFloat) -> Int {
     [
         2,
@@ -23,6 +22,7 @@ func evenDivisions(for height: CGFloat) -> Int {
         24 * 2, /// half hour
         24 * 4  /// quarter hour
     ]
+        /// choose whichever guarantees at least `minSize` to each division
         .filter{minSize * $0 < Int(height)}
-        .last ?? 1 /// default to 1 if no suitable number was found
+        .last ?? 1 /// default to 1
 }
