a968bdbf6d8e4f7b4c20489b3418b0adcdbb3434
Ipsky <yap@usc.edu>
2020-08-09 12:15:25 -0700

    create a special button class for the graph
    so we can add some extra aesthetic functions

--- LineGraph/GraphButton.swift
+++ LineGraph/GraphButton.swift
@@ -1,8 +1,8 @@
 //
-//  WeekButtonGlyph.swift
+//  GraphButton.swift
 //  Clok
 //
-//  Created by Secret Asian Man Dev on 24/6/20.
+//  Created by Secret Asian Man Dev on 9/8/20.
 //  Copyright © 2020 Secret Asian Man 3. All rights reserved.
 //
 
@@ -12,17 +12,16 @@ import SwiftUI
 // this was the only way I could think of to enforce width
 fileprivate let glyphFrameSize = CGFloat(10)
 fileprivate let backgroundPadding = CGFloat(15)
+fileprivate let radius = CGFloat(10)
 
-struct ButtonGlyph : ViewModifier {
-    let radius = CGFloat(10)
-    
-    func body(content: Content) -> some View {
-        content
+struct GraphButton: View {
+    var body: some View {
+        Image(systemName: "minus.magnifyingglass")
             .font(.system(size: glyphFrameSize * 2))
             // enforce square images so that SF symbols align vertically
             .frame(width: glyphFrameSize, height: glyphFrameSize)
-            // adapts to dark mode
-            .foregroundColor(.primary)
+            
+            .foregroundColor(.primary) /// adapts to dark mode
             .padding(backgroundPadding)
             .background(RaisedShape(radius: radius) { Circle() })
     }
