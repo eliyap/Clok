b0c4a2a5727e2ae2e2d55f01e5eeea1c00e01cb1
Ipsky <yap@usc.edu>
2020-08-09 11:42:09 -0700

    Update TimeIntervalExtension.swift
    properly declare these constants

--- Misc/Extensions/TimeIntervalExtension.swift
+++ Misc/Extensions/TimeIntervalExtension.swift
@@ -20,3 +20,9 @@ extension TimeInterval {
         return String(format: "%d:%02d:%02d", hh,mm,ss)
     }
 }
+
+extension TimeInterval {
+    static let hour = 60 * 60
+    static let day = 24 * hour
+    static let week = 7 * day
+}
