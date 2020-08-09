1ae42ef4f1625bf97ffc532599f4642d65af7daa
Ipsky <yap@usc.edu>
2020-08-08 19:54:57 -0700

    isolate graphmodel

--- LineGraph/BarStack.swift
+++ LineGraph/BarStack.swift
@@ -8,43 +8,6 @@
 
 import SwiftUI
 
-final class GraphModel: ObservableObject {
-    @Published var mode: Mode = .calendar
-    
-    /// what form this view is adopting
-    enum Mode {
-        case calendar
-        case graph
-        
-        mutating func toggle() -> Void {
-            switch self {
-            case .calendar: self = .graph
-            case .graph: self = .calendar
-            }
-        }
-    }
-    
-    /// how far back to look
-    var castBack: TimeInterval {
-        switch mode {
-        case .calendar: return dayLength * 0.5
-        case .graph: return dayLength * 0
-        }
-    }
-    
-    /// how far forwards to look
-    var castFwrd: TimeInterval {
-        switch mode {
-        case .calendar: return dayLength * 1.5
-        case .graph: return dayLength * 1
-        }
-    }
-    
-    var days: Double {
-        (castBack + castFwrd) / dayLength
-    }
-}
-
 struct BarStack: View {
     
     @EnvironmentObject private var bounds: Bounds
