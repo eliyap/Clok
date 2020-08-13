c31513985f4aee228f624f00919ce1ea0ca3f7f2
Ipsky <yap@usc.edu>
2020-08-13 09:31:36 -0700

    Update CGPointExtension.swift

--- Misc/Extensions/CGPoint/CGPointExtension.swift
+++ Misc/Extensions/CGPoint/CGPointExtension.swift
@@ -9,26 +9,4 @@
 import Foundation
 import SwiftUI
 
-extension CGPoint {
-    /**
-    distance to point from origin
-     */
-    func magnitude() -> CGFloat {
-        (x*x + y*y).squareRoot()
-    }
-    
-    /**
-     angle between +ve x-axis and the line from origion to point
-     angle is in radians
-     +ve angle is counter clockwise
-     */
-    func angle() -> Angle {
-        /// take the arctan, with coordinate zero at circle's center
-        var angle = Double(atan(y/x))
-        
-        angle += (y > 0 && x > 0) ? 0 : /// angle is in 1st quadrant, add nothing
-            (x < 0) ? Double.pi : /// 2nd & 3rd quadrant, add pi
-            2 * Double.pi /// 4th quardrant, add 2 pi
-        return Angle(radians: angle)
-    }
-}
+/// nothing here
