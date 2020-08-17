26235508cb5c47fe3d0dbe1a9686470e0ca3af59
Ipsky <yap@usc.edu>
2020-08-16 18:46:37 -0700

    Update combine flatMap learning.swift
    add some stuff to make it closer to my real use cse

--- Learning/combine flatMap learning.swift
+++ Learning/combine flatMap learning.swift
@@ -4,24 +4,30 @@ import Combine
 
 let intPublisher = [1,2,3].publisher
 
-let goalPublisher = Just(10)
+let goal = 100
+let goalPublisher = Just(goal)
 
-func createFuturePublisher(val: Int, deduct: Int) -> AnyPublisher<Int, Never> {
-    print("HP is: \(val)")
-    print("Rolled a \(deduct)")
-    let rem = val - deduct
-    guard rem > 0 else {
-        print("KO!")
+func createFuturePublisher(val: Int, add: Int) -> AnyPublisher<Int, Never> {
+    print("Total is: \(val)")
+    print("Rolled a \(add)")
+    let total = val + add
+    guard add != 1 else {
+        print("Rolled a 1, critical fail!")
         return Empty<Int, Never>()
             .eraseToAnyPublisher()
     }
-    return createFuturePublisher(val: rem, deduct: .random(in: 1...6))
+    guard goal > total else {
+        print("Goal Reached!")
+        return Empty<Int, Never>()
+            .eraseToAnyPublisher()
+    }
+    return createFuturePublisher(val: total, add: .random(in: 1...6))
         .eraseToAnyPublisher()
 }
 
 goalPublisher
-    .flatMap {
-        createFuturePublisher(val: $0, deduct: 0)
+    .flatMap { _ in
+        createFuturePublisher(val: 0, add: 0)
     }
     .sink(receiveValue: {
         print($0)
