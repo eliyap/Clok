e66272fb7607a73dddced8ad707ecaf3c69d231e
Ipsky <yap@usc.edu>
2020-08-21 11:11:42 -0700

    org netcode

---
+++ NetCode/Fetchers/FetchRunningEntry.swift
@@ -0,0 +1,16 @@
+//
+//  FetchRunningEntry.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 21/8/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+final class RunningEntryLoader: ObservableObject {
+    var loader: AnyCancellable? = nil
+    
+    
+}
