2e7cb879cc2e012d18a1e29532a51cc44ba58971
Ipsky <yap@usc.edu>
2020-08-23 15:58:31 -0700

    Update Tag+CoreDataClass.swift

--- Data/Tag+CoreDataClass.swift
+++ Data/Tag+CoreDataClass.swift
@@ -10,7 +10,7 @@
 import Foundation
 import CoreData
 
-fileprivate struct RawTag: Decodable {
+struct RawTag: Decodable {
     /// name, guaranteed unique among within the workspace,
     /// https://github.com/toggl/toggl_api_docs/blob/b19c3b61f2b1be2eeccc28ea4e6acee38cfc72a1/chapters/tags.md#tags
     let name: String
@@ -34,12 +34,9 @@ public class Tag: NSManagedObject {
         super.init(entity: entity, insertInto: context)
     }
     
-    public required init(from decoder: Decoder) throws {
-        guard let context = decoder.userInfo[.context] as? NSManagedObjectContext else { fatalError("NSManagedObjectContext is missing") }
-
+    init(from rawTag: RawTag, context: NSManagedObjectContext) throws {
         super.init(entity: Tag.entity(), insertInto: context)
         
-        let rawTag = try RawTag(from: decoder)
         id = Int64(rawTag.id)
         wid = Int64(rawTag.wid)
         name = rawTag.name
