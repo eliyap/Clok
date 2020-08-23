4af20113bf1c93837d6bf69bee120ec44e72d2a0
Ipsky <yap@usc.edu>
2020-08-23 15:46:49 -0700

    add Tag Core Data Model

---
+++ Data/Tag+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Tag+CoreDataClass.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 23/8/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Tag)
+public class Tag: NSManagedObject {
+
+}
