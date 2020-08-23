334bc2f3dadec81c4997b115f9f2ec8bff704366
Ipsky <yap@usc.edu>
2020-08-23 15:46:49 -0700

    add Tag Core Data Model

---
+++ Data/Tag+CoreDataProperties.swift
@@ -0,0 +1,46 @@
+//
+//  Tag+CoreDataProperties.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 23/8/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Tag {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Tag> {
+        return NSFetchRequest<Tag>(entityName: "Tag")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var id: Int64
+    @NSManaged public var wid: Int64
+    @NSManaged public var entries: NSSet?
+
+}
+
+// MARK: Generated accessors for entries
+extension Tag {
+
+    @objc(addEntriesObject:)
+    @NSManaged public func addToEntries(_ value: TimeEntry)
+
+    @objc(removeEntriesObject:)
+    @NSManaged public func removeFromEntries(_ value: TimeEntry)
+
+    @objc(addEntries:)
+    @NSManaged public func addToEntries(_ values: NSSet)
+
+    @objc(removeEntries:)
+    @NSManaged public func removeFromEntries(_ values: NSSet)
+
+}
+
+extension Tag : Identifiable {
+
+}
