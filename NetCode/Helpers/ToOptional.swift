e0c531d1209da2c046100aa3babeef92198507dc
Ipsky <yap@usc.edu>
2020-08-23 16:42:22 -0700

    write some generic functions

---
+++ NetCode/Helpers/ToOptional.swift
@@ -0,0 +1,15 @@
+//
+//  ToOptional.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 23/8/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import Foundation
+
+/// return input as an optional
+/// used with Combine to change publisher type to optional
+func toOptional<T>(input: T) -> T? {
+    input
+}
