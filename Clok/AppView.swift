292d542a605aeae49524e59b42fc1a2c60305f65
Ipsky <yap@usc.edu>
2020-08-23 18:06:22 -0700

    rewrite code to be more parallel

--- Clok/AppView.swift
+++ Clok/AppView.swift
@@ -63,7 +63,7 @@ struct ClokApp: App {
                 .environmentObject(projectLoader)
                 .environment(\.managedObjectContext, persistentContainer.viewContext)
                 /// update on change to either user or space
-                /// also fires at app launch when user is logged in
+                /// NOTE: also fires at app launch if the user is logged in
                 .onReceive(cred.$user) { user in
                     guard let user = user else { return }
                     #if DEBUG
@@ -80,17 +80,28 @@ struct ClokApp: App {
                     print("Detailed report for \(date) requested")
                     #endif
                     guard let user = cred.user else { return }
-                    entryLoader.fetchEntries(
-                        /// NOTE: add a 1 day margin of safety on either side
-                        range: (
-                            start: date - .day,
-                            end: date + .week + .day
-                        ),
-                        user: user,
-                        projects: loadProjects(context: persistentContainer.viewContext) ?? [],
-                        tags: loadTags(context: persistentContainer.viewContext) ?? [],
-                        context: persistentContainer.viewContext
+                    
+                    /// NOTE: add a 1 day margin of safety on either side
+                    let range = (
+                        start: date - .day,
+                        end: date + .week + .day
                     )
+                    
+                    entryLoader.loader = entryLoader.fetchRawEntries(
+                        range: range,
+                        user: user
+                    )
+                    /// switch to main thread before performing CoreData work
+                    .receive(on: DispatchQueue.main)
+                    .sink {
+                        entryLoader.saveEntries(
+                            range: range,
+                            rawEntries: $0,
+                            projects: loadProjects(context: persistentContainer.viewContext) ?? [],
+                            tags: loadTags(context: persistentContainer.viewContext) ?? [],
+                            context: persistentContainer.viewContext
+                        )
+                    }
                 })
         }
     }
