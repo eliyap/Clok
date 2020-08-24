8bdcc0496ab85f92105811b6271736214e84c0e5
Ipsky <yap@usc.edu>
2020-08-23 18:06:22 -0700

    rewrite code to be more parallel

--- NetCode/Fetchers/EntryLoader.swift
+++ NetCode/Fetchers/EntryLoader.swift
@@ -12,7 +12,7 @@ import CoreData
 import SwiftUI
 
 final class EntryLoader: ObservableObject {
-    private var loader: AnyCancellable? = nil
+    var loader: AnyCancellable? = nil
     
     /// represents the state of the `EntryLoader`
     enum LoadState: Int {
@@ -30,14 +30,11 @@ final class EntryLoader: ObservableObject {
     @Published var totalCount = 0
     @Published var loaded = 0
     
-    func fetchEntries(
+    func fetchRawEntries(
         range: DateRange,
         user: User,
-        projects: [Project],
-        tags: [Tag],
-        context: NSManagedObjectContext,
         initialLogin: Bool = false
-    ) -> Void {
+    ) -> AnyPublisher<[RawTimeEntry]?, Never> {
         /// begin loading animations
         withAnimation {
             state = initialLogin ? .loadingLengthy : .loadingBriefly
@@ -55,46 +52,55 @@ final class EntryLoader: ObservableObject {
             "until=\(df.string(from: range.end))"
         ].joined(separator: "&")
     
-        loader = recursiveLoadPages(
+        return recursiveLoadPages(
             api_string: api_string,
             auth: auth(token: user.token)
         )
-            /// switch to main thread before performing CoreData work
-            .receive(on: DispatchQueue.main)
             .map(toOptional)
             .catch(printAndReturnNil)
-            .sink(receiveValue: { (rawEntries: [RawTimeEntry]?) in
-                /// terminate if error resulted in `nil` being passed
-                guard let rawEntries = rawEntries else { return }
-                
-                /// fetch the CoreData records for this `DateRange`
-                var entries = loadEntries(range: range, context: context)
-                
-                /// perform matching between new and stored data
-                rawEntries.forEach { (rawEntry: RawTimeEntry) in
-                    if let entryIdx = entries?.firstIndex(where: {$0.id == rawEntry.id}) {
-                        /// if a matching `TimeEntry` was found, update the record
-                        entries![entryIdx].update(from: rawEntry, projects: projects, tags: tags)
-                        /// and remove it from future consideration
-                        entries!.remove(at: entryIdx)
-                    } else {
-                        /// if no match was found, insert the new `TimeEntry`
-                        context.insert(TimeEntry(from: rawEntry, context: context, projects: projects, tags: tags))
-                    }
-                }
-                /**
-                 Remaining `TimeEntry`s had no match in the fetched data.
-                 Hence they must have been deleted, so we will delete them in CoreData as well.
-                 */
-                for deletedEntry in entries ?? [] {
-                    context.delete(deletedEntry)
-                }
-                try! context.save()
-                
-                /// loading complete!
-                withAnimation {
-                    self.state = .finished
-                }
-            })
+            .eraseToAnyPublisher()
+    }
+    
+    /**
+     Save provided `RawTimeEntry`s to CoreData
+     */
+    func saveEntries(
+        range: DateRange,
+        rawEntries: [RawTimeEntry]?,
+        projects: [Project],
+        tags: [Tag],
+        context: NSManagedObjectContext
+    ) -> Void {
+        /// terminate if error resulted in `nil` being passed
+        guard let rawEntries = rawEntries else { return }
+        
+        /// fetch the CoreData records for this `DateRange`
+        var entries = loadEntries(range: range, context: context)
+        
+        /// perform matching between new and stored data
+        rawEntries.forEach { (rawEntry: RawTimeEntry) in
+            if let entryIdx = entries?.firstIndex(where: {$0.id == rawEntry.id}) {
+                /// if a matching `TimeEntry` was found, update the record
+                entries![entryIdx].update(from: rawEntry, projects: projects, tags: tags)
+                /// and remove it from future consideration
+                entries!.remove(at: entryIdx)
+            } else {
+                /// if no match was found, insert the new `TimeEntry`
+                context.insert(TimeEntry(from: rawEntry, context: context, projects: projects, tags: tags))
+            }
+        }
+        /**
+         Remaining `TimeEntry`s had no match in the fetched data.
+         Hence they must have been deleted, so we will delete them in CoreData as well.
+         */
+        for deletedEntry in entries ?? [] {
+            context.delete(deletedEntry)
+        }
+        try! context.save()
+        
+        /// loading complete!
+        withAnimation {
+            self.state = .finished
+        }
     }
 }
