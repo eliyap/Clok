9e29eea5d1df04586f7c744a09d3bd66216f0c75
Ipsky <yap@usc.edu>
2020-09-03 16:31:07 -0700

    new stuff

---
+++ ClokWidget/Summary.swift
@@ -0,0 +1,13 @@
+//
+//  Summary.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 3/9/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import Foundation
+
+struct RawSummary: Decodable {
+    
+}
---
+++ ClokWidget/SummaryLoader.swift
@@ -0,0 +1,12 @@
+//
+//  SummaryLoader.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 3/9/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+
