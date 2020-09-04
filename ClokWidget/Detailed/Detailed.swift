f5b58faddfa37515fee248185d52885091848628
Ipsky <yap@usc.edu>
2020-09-03 18:35:18 -0700

    new folder

---
+++ ClokWidget/Detailed/Detailed.swift
@@ -0,0 +1,9 @@
+//
+//  Detailed.swift
+//  ClokWidgetExtension
+//
+//  Created by Secret Asian Man Dev on 3/9/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import Foundation
