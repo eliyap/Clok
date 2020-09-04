f9a8f0f329e7c4e436e4d29420e88a540d47d702
Ipsky <yap@usc.edu>
2020-09-03 18:37:25 -0700

    Update Detailed.swift

--- ClokWidget/Detailed/Detailed.swift
+++ ClokWidget/Detailed/Detailed.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+func fetchDetailed(
+    token: String,
+    wid: Int,
+    completion:@escaping (Summary?, Error?) -> Void
+) -> Void {
+    /// API URL documentation:
+    /// https://github.com/toggl/toggl_api_docs/blob/master/reports/summary.md
+    let url = [
+        "\(REPORT_URL)/summary?user_agent=\(user_agent)",
+        "workspace_id=\(wid)",
+        "since=\(Date().midnight.iso8601)",
+        "end=\(Date().midnight.advanced(by: .day).iso8601)"
+    ].joined(separator: "&")
+    
+    let request = formRequest(
+        url: URL(string: url)!,
+        auth: auth(token: token)
+    )
+    
+    URLSession.shared.dataTask(with: request) { (data, response, error) -> Void in
+        guard error == nil else {
+            completion(nil, error!)
+            return
+        }
+        guard let data = data else {
+            completion(nil, NetworkError.other)
+            return
+        }
+        let code = (response as? HTTPURLResponse)?.statusCode ?? -1
+        guard 200...299 ~= code else {
+            completion(nil, NetworkError.statusCode(code: code))
+            return
+        }
+        do {
+            let summary = try JSONDecoder(dateStrategy: .iso8601).decode(Summary.self, from: data)
+            completion(summary, nil)
+        } catch {
+            completion(nil, NetworkError.serialization)
+        }
+    }.resume()
+}
