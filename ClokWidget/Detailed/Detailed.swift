996918fc1d3fbd18b75845b58578bf151ba5d326
Ipsky <yap@usc.edu>
2020-09-03 18:41:32 -0700

    Revert "new folder"
    This reverts commit f5b58faddfa37515fee248185d52885091848628.

--- ClokWidget/Detailed/Detailed.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Detailed.swift
-//  ClokWidgetExtension
-//
-//  Created by Secret Asian Man Dev on 3/9/20.
-//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
-//
-
-import Foundation
