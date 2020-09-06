ab9d8bae0e16596880ee2de090b111fac2f61072
Ipsky <yap@usc.edu>
2020-09-05 20:13:12 -0700

    prototype widget view

--- ClokWidget/WidgetView.swift
+++ ClokWidget/WidgetView.swift
@@ -9,6 +9,11 @@
 import Foundation
 import SwiftUI
 
+fileprivate let strokeStyle = StrokeStyle(
+    lineWidth: 4,
+    lineCap: .round
+)
+
 struct ClokWidgetEntryView : View {
     
     @Environment(\.widgetFamily) var family
@@ -16,7 +21,7 @@ struct ClokWidgetEntryView : View {
     var entry: SummaryEntry
     
     var body: some View {
-        return GeometryReader{ geo in
+        GeometryReader{ geo in
             if entry.summary == .noSummary {
                 Text("Nothing Here")
             } else {
@@ -32,21 +37,54 @@ struct ClokWidgetEntryView : View {
                 entry.summary.projects.sorted(by: {$0.duration > $1.duration}),
                 id: \.id
             ) { project in
-                HStack {
-                    Rings(project: project)
-                    Text(project.name)
-                        .foregroundColor(project.color)
-                        .font(.caption)
-                        .layoutPriority(1)
+                GeometryReader { geo in
+                    HStack {
+                        Rings(project: project, size: geo.size)
+                        Text(project.name)
+                            .foregroundColor(project.color)
+                            .font(.caption)
+                            .layoutPriority(1)
+                    }
                 }
             }
         }
     }
     
-    func Rings(project: Summary.Project) -> some View {
-        ForEach(0..<Int(project.duration / .hour), id: \.self) {_ in
-            Circle()
+    func Rings(project: Summary.Project, size: CGSize) -> some View {
+        let remainder = project.duration.remainder(dividingBy: .hour) / .hour
+        return Group {
+            ForEach(0..<Int(project.duration / .hour), id: \.self) {_ in
+                Circle()
+                    .stroke(style: strokeStyle)
+                    .frame(width: size.height, height: size.height)
+                    .foregroundColor(project.color)
+            }
+            Arc(angle: Angle(radians: .tau * remainder))
+                .frame(width: size.height, height: size.height)
                 .foregroundColor(project.color)
+        }
+    }
+}
+
+struct Arc: Shape {
+    
+    let angle: Angle
+    
+    func path(in rect: CGRect) -> Path {
+        return Path { path in
+            path.move(to: CGPoint(x: rect.width / 2, y: .zero))
+            path.addRelativeArc(
+                center: CGPoint(x: rect.width / 2, y: rect.height / 2),
+                radius: rect.width / 2,
+                startAngle: Angle(radians: -.pi / 2),
+                delta: angle
+            )
+            path.addRelativeArc(
+                center: CGPoint(x: rect.width / 2, y: rect.height / 2),
+                radius: rect.width / 2,
+                startAngle: angle,
+                delta: -angle
+            )
         }
     }
 }
