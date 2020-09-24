240f0abd74fe460c5d998540bbaf77039f68dffa
Ipsky <yap@usc.edu>
2020-09-24 10:58:22 -0700

    Update TokenWebView.swift

--- Misc/LoginScreen/TokenWebView.swift
+++ Misc/LoginScreen/TokenWebView.swift
@@ -7,9 +7,24 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct TokenWebView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WebView()
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    
+    typealias UIViewType = WKWebView
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.load(URLRequest(url: URL(string: "https://www.google.com")!))
+        return webView
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
     }
 }
