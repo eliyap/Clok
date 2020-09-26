5d93d95478ba3f3a2a58baaf69e1a42ca98c0f1c
Ipsky <yap@usc.edu>
2020-09-25 19:41:12 -0700

    allow widget to make web request

--- Data/Project+CoreDataClass.swift
+++ Data/Project+CoreDataClass.swift
@@ -11,24 +11,6 @@ import Foundation
 import SwiftUI
 import CoreData
 
-/// simplify decoding
-/// Documentation:
-/// https://github.com/toggl/toggl_api_docs/blob/f62b8f4bb9118d97af54df48a268ff1e4319b34b/chapters/projects.md#projects
-struct RawProject: Decodable {
-    let id: Int
-    let is_private: Bool
-    let wid: Int
-    let hex_color: String
-    let name: String
-    let billable: Bool
-    
-    /// `Date` the "task" was last updated
-    let at: Date
-    // one of these cases a coding failure, ignore for now
-//        var actual_hours: Int
-//        var color: Int // probably an enum for toggl's default color palette
-}
-
 @objc(Project)
 public class Project: NSManagedObject, Decodable, ProjectLike {
     
--- ProjectIntent/IntentHandler.swift
+++ ProjectIntent/IntentHandler.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Secret Asian Man 3. All rights reserved.
 //
 
+import WidgetKit
 import Intents
 
 class IntentHandler: INExtension, ClokConfigurationIntentHandling {
@@ -18,12 +19,49 @@ class IntentHandler: INExtension, ClokConfigurationIntentHandling {
     }
     
     func provideProjectOptionsCollection(for intent: ClokConfigurationIntent, with completion: @escaping (INObjectCollection<IntentProject>?, Error?) -> Void) {
-        let item = IntentProject(identifier: "a thing", display: "a thong")
+        // fetch credentials from Keychain
+        guard let (_, _, token, chosenWID) = try? getKey() else {
+            completion(INObjectCollection(items: []), nil)
+            return
+        }
+        
+        /// API URL documentation:
+        /// https://github.com/toggl/toggl_api_docs/blob/master/chapters/workspaces.md#get-workspace-projects
+        let request = formRequest(
+            url: URL(string: "\(API_URL)/workspaces/\(chosenWID)/projects?user_agent=\(user_agent)")!,
+            auth: auth(token: token)
+        )
+        
+        /// fetch and decode RawProjects
+        URLSession.shared.dataTask(with: request) { (data, response, error) -> Void in
+            guard error == nil else {
+                completion(INObjectCollection(items: []), nil)
+                return
+            }
+            guard let data = data else {
+                completion(INObjectCollection(items: []), nil)
+                return
+            }
+            let code = (response as? HTTPURLResponse)?.statusCode ?? -1
+            guard 200...299 ~= code else {
+                completion(INObjectCollection(items: []), nil)
+                return
+            }
+            do {
+                let raws = try JSONDecoder(dateStrategy: .iso8601).decode([RawProject].self, from: data)
+                completion(INObjectCollection(items:raws.map{
+                    let proj = IntentProject(
+                        identifier: "\($0.id)",
+                        display: $0.name
+                    )
+                    proj.pid = NSNumber(value: $0.id)
+                    return proj
+                }), nil)
+            } catch {
+                completion(INObjectCollection(items: []), nil)
+            }
+        }.resume()
         
         
-        
-        let collection = INObjectCollection(items: [item])
-        completion(collection, nil)
     }
-    
 }
