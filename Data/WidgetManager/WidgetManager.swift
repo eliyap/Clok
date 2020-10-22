dc8c2af01402161ef8c708765ade21ffff8a9c66
Ipsky <yap@usc.edu>
2020-10-22 08:34:07 -0700

    create userdefaults shared store for widgets

---
+++ Data/WidgetManager/WidgetManager.swift
@@ -0,0 +1,13 @@
+//
+//  WidgetManager.swift
+//  RunningRingExtension
+//
+//  Created by Secret Asian Man Dev on 22/10/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import Foundation
+struct WidgetManager {
+    static let suiteName = "widgets.sam.clok"
+    static let suite = UserDefaults(suiteName: suiteName)
+}
