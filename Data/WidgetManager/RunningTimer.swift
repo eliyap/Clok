c817dd5779071f95baa0a9665cdca6b2bbfacd39
Ipsky <yap@usc.edu>
2020-10-22 10:52:18 -0700

    create file

---
+++ Data/WidgetManager/RunningTimer.swift
@@ -0,0 +1,18 @@
+//
+//  RunningTimer.swift
+//  RunningRingExtension
+//
+//  Created by Secret Asian Man Dev on 22/10/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import Foundation
+
+//extension WidgetManager {
+//    //MARK:- Running Timer
+//    private static let runningKey = "ClokWidgets.RunningTimer"
+//    /// currently running timer information
+//    static var running: RunningEntry? {
+//        
+//    }
+//}
