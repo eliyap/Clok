d4b9c502f5b23d920da8a33e9285e6ebb9e9f2d1
Ipsky <yap@usc.edu>
2020-11-28 14:00:35 +0800

    edit scope

---
+++ Report/Detailed/IntegrateRunningEntry.swift
@@ -0,0 +1,15 @@
+//
+//  IntegrateRunningEntry.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 28/11/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import Foundation
+
+extension Detailed {
+    func fillIn(_ raw: RawRunningEntry) {
+        
+    }
+}
