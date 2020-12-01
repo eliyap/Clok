b74bc95811a4f90c6bab36fe16ebd9b6009c8e86
Ipsky <yap@usc.edu>
2020-12-01 12:18:16 +0800

    Update DayCalendar.swift

--- NewGraph/Views/Calendar/DayCalendar.swift
+++ NewGraph/Views/Calendar/DayCalendar.swift
@@ -10,7 +10,40 @@ import Foundation
 import SwiftUI
 
 struct DayCalendar: View {
+    
+    @EnvironmentObject var data: TimeData
+    @EnvironmentObject var model: NewGraphModel
+    @FetchRequest(
+        entity: TimeEntry.entity(),
+        sortDescriptors: []
+    ) var entries: FetchedResults<TimeEntry>
+    
+    let dayHeight: CGFloat     /// visual height for 1 day
+    let start: Date
+    let animationInfo: (namespace: Namespace.ID, row: Int)
+    
     var body: some View {
-        
+        NewDayStrip(
+            entries: timeEntries,
+            midnight: start,
+            terms: data.terms,
+            animationInfo: (
+                animationInfo.namespace,
+                animationInfo.row,
+                start.timeIntervalSince1970
+            )
+        )
+            .frame(height: dayHeight)
+            /// NOTE: apply lined background to whole stack, NOT individual `DayStrip`!
+            .background(NewLinedBackground(divisions: evenDivisions(for: dayHeight)))
+            .drawingGroup()
+    }
+    
+    var timeEntries: [TimeEntry] {
+        entries
+            .filter{$0.end > start}
+            .filter{$0.start < start + .day}
+            /// chronological sort
+            .sorted{$0.start < $1.start}
     }
 }
