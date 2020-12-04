692107405815dc44653e518cbc85230a2e97467d
Ipsky <yap@usc.edu>
2020-12-04 11:42:25 +0800

    backup

--- FlexibleGraph/EntryFullScreenModal.swift
+++ FlexibleGraph/EntryFullScreenModal.swift
@@ -10,35 +10,44 @@ import SwiftUI
 
 extension FlexibleGraph {
     var EntryFullScreenModal: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
-                HStack {
-                    VStack(alignment: .leading) {
-                        Button {
+        GeometryReader { geo in
+            VStack(alignment: .leading, spacing: .zero) {
+                ScrollView(.vertical) {
+                    LazyVStack(alignment: .leading, spacing: .zero, pinnedViews: .sectionHeaders) {
+                        Section(header: Button {
                             withAnimation { showEntry = false }
                         } label: {
                             Image(systemName: "xmark")
                         }
                             .buttonStyle(PlainButtonStyle())
-                        Text(model.selected?.entry.wrappedDescription ?? "[No Description]")
-                            .font(.title)
-                        Label(model.selected?.entry.projectName ?? StaticProject.noProject.name, systemImage: "folder.fill")
-                        Label(model.selected?.entry.dur.toString() ?? placeholderTime, systemImage: "stopwatch")
+                            .padding()
+                            .frame(width: geo.size.width, alignment: .leading)
+                            .background(Color(UIColor.secondarySystemFill))
+                        ) {
+                            VStack(alignment: .leading) {
+                                Text(model.selected?.entry.wrappedDescription ?? "[No Description]")
+                                    .font(.title)
+                                Label(model.selected?.entry.projectName ?? StaticProject.noProject.name, systemImage: "folder.fill")
+                                Label(model.selected?.entry.dur.toString() ?? placeholderTime, systemImage: "stopwatch")
+                                Spacer()
+                            }
+                                .padding()
+                                .frame(width: geo.size.width, alignment: .leading)
+                                .background(model.selected?.entry.color)
+                            
+                        }
+                        VStack {
+                            Label(model.selected?.entry.projectName ?? StaticProject.noProject.name, systemImage: "folder.fill")
+                            Label(model.selected?.entry.dur.toString() ?? placeholderTime, systemImage: "stopwatch")
+                            Spacer()
+                        }
+                            .padding()
                     }
-                    Spacer()
+                        .background(Color.background)
+                        .matchedGeometryEffect(id: model.selected!, in: namespace, isSource: showEntry)
                 }
-                    .padding()
-                    .background(model.selected?.entry.color)
-                VStack {
-                    Label(model.selected?.entry.projectName ?? StaticProject.noProject.name, systemImage: "folder.fill")
-                    Label(model.selected?.entry.dur.toString() ?? placeholderTime, systemImage: "stopwatch")
-                }
-                    .padding()
-                    .background(Color.background)
-            
             }
-                .matchedGeometryEffect(id: model.selected!, in: namespace, isSource: showEntry)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
+            .background(Color(UIColor.systemGray5))
     }
 }
