9eb93cada92fe2ff3ecd6ea1a58e059ff2562838
Ipsky <yap@usc.edu>
2020-12-05 14:54:15 +0800

    define class

---
+++ Misc/AppTimer.swift
@@ -0,0 +1,30 @@
+//
+//  AppTimer.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 5/12/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+/** A `Publisher` disguised as an `ObservableObject`
+ This hack is borne because `onReceive` is not allowed on `WindowGroup`,
+ but `onChange` is, strangely.
+ Hence a publisher that periodically changes a value was needed.
+ This is desirable so that multiple windows are not collectively clobbering the Toggl servers with periodic requests
+ */
+class AppTimer: ObservableObject {
+    var cancellable: AnyCancellable? = nil
+    @Published var _t: Bool = false
+    init() {
+        cancellable = Timer
+            .publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink(receiveValue: { _ in
+                self._t.toggle()
+            })
+    }
+}
+
