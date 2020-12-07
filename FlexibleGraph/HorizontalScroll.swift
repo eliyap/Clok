16c6688f2e233ad3bb0710f05330f9848bfac37b
Ipsky <yap@usc.edu>
2020-12-07 17:55:03 +0800

    POC vertical scroller

--- FlexibleGraph/HorizontalScroll.swift
+++ FlexibleGraph/HorizontalScroll.swift
@@ -9,7 +9,22 @@
 import SwiftUI
 
 extension FlexibleGraph {
+    
     var HorizontalScroll: some View {
+        GeometryReader { geo in
+            HStack(spacing: .zero) {
+                NewTimeIndicator(divisions: evenDivisions(for: geo.size.height))
+                    .gesture(DragGesture()
+                        .onChanged { state in
+                            rowPosition.position.y += state.translation.height / geo.size.height
+                        }
+                    )
+                HorizontalScrollView
+            }
+        }
+    }
+    
+    var HorizontalScrollView: some View {
         /** Enables user to scroll left (but not right) as far as they want.
          Important to my vision of allowing cross-day visualization.
          Built on a monstrous hack that exploits the behaviour of `LazyHStack`,
@@ -22,8 +37,12 @@ extension FlexibleGraph {
                         ForEach(RowList, id: \.self) { idx in
                             
                             WeekStrip(
-                                midnight: Date().midnight.advanced(by: Double(idx) * .day),
+                                midnight: Date()
+                                    .midnight
+                                    /// NOTE: -1 is a magic number to correct for offset
+                                    .advanced(by: Double(idx) * .day + Double(rowPosition.position.y) * .day),
                                 row: idx,
+                                /// NOTE: col is somewhat meaningless and does NOT match midnight
                                 col: Date().midnight.advanced(by: Double(idx) * .day).timeIntervalSince1970
                             )
                                 .frame(
