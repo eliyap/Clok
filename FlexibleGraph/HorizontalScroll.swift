01da2c2b078e6a5cbea1e258a38ae50b91ac3089
Ipsky <yap@usc.edu>
2020-12-07 15:05:24 +0800

    new view

---
+++ FlexibleGraph/HorizontalScroll.swift
@@ -0,0 +1,15 @@
+//
+//  HorizontalScroll.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 7/12/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import SwiftUI
+
+extension FlexibleGraph {
+    var HorizontalScroll: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
