51bf90e32a919eda33f66f2b019765e99f1128a9
Ipsky <yap@usc.edu>
2020-12-07 12:49:34 +0800

    split off view

--- FlexibleGraph/FlexibleGraph.swift
+++ FlexibleGraph/FlexibleGraph.swift
@@ -94,30 +94,7 @@ struct FlexibleGraph: View {
                     /// increase zIndex so that, while animating, modal does not fall behind other entries
                     .zIndex(1)
             }
-            (passthroughSelected?.color ?? .clear)
-                .matchedGeometryEffect(
-                    id: passthroughGeometry ?? NamespaceModel.none,
-                    /// switch namespaces when entry is selected  / de-selected
-                    in: model.selected == nil ? graphNamespace : modalNamespace,
-                    isSource: false
-                )
-                /** Passes Changes on, but `withAnimation`
-                 The key to this approach is that after a `TimeEntry` is selected,
-                 the view instantly (without animation) snaps to the `TimeEntry`,
-                 then it switches `Namespace`s and targets `EntryFullScreenModal` to create the animation.
-                 It has the advantage of being `ZStack`ed above the graph, and does not clip behind any other entries
-                 */
-                .onChange(of: passthroughSelected) { newSelection in
-                    withAnimation {
-                        model.selected = newSelection
-                    }
-                    
-                }
-                .onChange(of: passthroughGeometry) { newGeometry in
-                    withAnimation {
-                        model.geometry = newGeometry
-                    }
-                }
+            MGEAnimator
         }
     }
 }
---
+++ FlexibleGraph/MGEAnimator.swift
@@ -0,0 +1,38 @@
+//
+//  MGEAnimator.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 7/12/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import SwiftUI
+
+extension FlexibleGraph {
+    var MGEAnimator: some View {
+        (passthroughSelected?.color ?? .clear)
+            .matchedGeometryEffect(
+                id: passthroughGeometry ?? NamespaceModel.none,
+                /// switch namespaces when entry is selected  / de-selected
+                in: model.selected == nil ? graphNamespace : modalNamespace,
+                isSource: false
+            )
+            /** Passes Changes on, but `withAnimation`
+             The key to this approach is that after a `TimeEntry` is selected,
+             the view instantly (without animation) snaps to the `TimeEntry`,
+             then it switches `Namespace`s and targets `EntryFullScreenModal` to create the animation.
+             It has the advantage of being `ZStack`ed above the graph, and does not clip behind any other entries
+             */
+            .onChange(of: passthroughSelected) { newSelection in
+                withAnimation {
+                    model.selected = newSelection
+                }
+                
+            }
+            .onChange(of: passthroughGeometry) { newGeometry in
+                withAnimation {
+                    model.geometry = newGeometry
+                }
+            }
+    }
+}
