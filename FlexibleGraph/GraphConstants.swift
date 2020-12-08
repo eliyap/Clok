6890e8c5a375b5550bcd19d70524f99a3a6c705d
Ipsky <yap@usc.edu>
2020-12-08 10:53:38 +0800

    add constants file

---
+++ FlexibleGraph/GraphConstants.swift
@@ -0,0 +1,14 @@
+//
+//  GraphConstants.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 8/12/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import Foundation
+
+enum GraphConstants {
+    /// the number of days showen in `.extendedMode`
+    static let dayCount = 7
+}
