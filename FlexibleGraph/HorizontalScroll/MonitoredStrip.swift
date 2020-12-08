e1829a8d833de56c02c6fcb90765f042fcf52e3b
Ipsky <yap@usc.edu>
2020-12-08 14:05:42 +0800

    split off func

--- FlexibleGraph/HorizontalScroll/MonitoredStrip.swift
+++ FlexibleGraph/HorizontalScroll/MonitoredStrip.swift
@@ -21,28 +21,44 @@ extension FlexibleGraph {
                 DayStrip(midnight: midnight, idx: idx)
             }
                 .frame(width: geo.size.width)
-                .onReceive(positionRequester) { rowAdjustment in
-                    let trailingOffset = trailing - geo.frame(in: .global).maxX
-
-                    /// conditional ensures that only the strip actually intersecting the trailing edge of the screen will report its position
-                    if trailingOffset.isBetween(0, geo.size.width) {
-                        rowPosition.row = idx
-                        rowPosition.position.x = trailingOffset / geo.size.width
-                    }
-                    
-                    /// adjust row, if requested
-                    guard let rowAdjustment = rowAdjustment else {
-                        return
-                    }
-                    
-                    proxy.scrollTo(
-                        rowPosition.row + 1 + rowAdjustment,
-                        anchor: UnitPoint(
-                            x: GraphConstants.hProp * (1-rowPosition.position.x),
-                            y: 0
-                        )
-                    )
-                }
+                .onReceive(positionRequester) { adjustRow(trailing: trailing, geo: geo, proxy: proxy, idx: idx, rowAdjustment: $0) }
         }
+    }
+    
+    
+    /// Responsds to a request to get and potentially set the `ScrollView`'s position
+    /// - Parameters:
+    ///   - trailing: distace to the trailing edge of the parent `ScrollView` in `.global` coordinates
+    ///   - geo: `GeometryProxy` of this column
+    ///   - proxy: `ScrollViewProxy` of the parent `ScrollView`, allowing us to set a new position
+    ///   - idx: index of this column
+    ///   - rowAdjustment: requested change to the scroll position
+    func adjustRow(
+        trailing: CGFloat,
+        geo: GeometryProxy,
+        proxy: ScrollViewProxy,
+        idx: Int,
+        rowAdjustment: Int?
+    ) -> Void {
+        let trailingOffset = trailing - geo.frame(in: .global).maxX
+
+        /// conditional ensures that only the strip actually intersecting the trailing edge of the screen will report its position
+        if trailingOffset.isBetween(0, geo.size.width) {
+            rowPosition.row = idx
+            rowPosition.position.x = trailingOffset / geo.size.width
+        }
+        
+        /// adjust row, if requested
+        guard let rowAdjustment = rowAdjustment else {
+            return
+        }
+        
+        proxy.scrollTo(
+            rowPosition.row + 1 + rowAdjustment,
+            anchor: UnitPoint(
+                x: GraphConstants.hProp * (1-rowPosition.position.x),
+                y: 0
+            )
+        )
     }
 }
