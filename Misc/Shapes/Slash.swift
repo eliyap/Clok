fe5364ded2d9c75f49aa24903d4f46aea5f08993
Ipsky <yap@usc.edu>
2020-12-11 13:37:48 +0800

    add slash

---
+++ Misc/Shapes/Slash.swift
@@ -0,0 +1,68 @@
+//
+//  Slash.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 11/12/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import SwiftUI
+
+struct Slash: View {
+    
+    /// note: Apple's SF `location.slash` uses this ratio
+    static let strokeRatio: CGFloat = 0.75
+    
+    var strokeWidth: CGFloat = 1.2
+    var length: CGFloat = 1
+    var body: some View {
+        ZStack {
+            Strike(strokeWidth: strokeWidth * (1 + Self.strokeRatio), length: length)
+                .strokeBorder(style: StrokeStyle(
+                    lineWidth: strokeWidth * Self.strokeRatio,
+                    lineCap: .round,
+                    lineJoin: .round
+                ))
+                .foregroundColor(.white)
+            Strike(strokeWidth: strokeWidth, length: length)
+                .foregroundColor(.black)
+        }
+    }
+}
+
+struct Strike: InsettableShape {
+    
+    var insetAmount: CGFloat = .zero
+    var strokeWidth: CGFloat = .zero
+    var length: CGFloat = 1
+    
+    var animatableData: CGFloat {
+        get { length }
+        set { length = newValue }
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var line = self
+        line.insetAmount += amount
+        return line
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        guard length > 0 else {
+            return Path()
+        }
+        
+        return Path { path in
+            path.move(to: .zero)
+            path.addLine(to: CGPoint(
+                x: rect.width * length,
+                y: rect.height * length
+            ))
+        }
+        .strokedPath(StrokeStyle(
+            lineWidth: strokeWidth,
+            lineCap: .round,
+            lineJoin: .round
+        ))
+    }
+}
