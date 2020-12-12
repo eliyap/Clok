a1949868482c00603fb198c4a24c1730238341f3
Ipsky <yap@usc.edu>
2020-12-12 18:36:04 +0800

    save raws as raws

--- Data/ProjectPresets.swift
+++ Data/ProjectPresets.swift
@@ -9,62 +9,20 @@
 import Foundation
 import CoreData
 
-extension Project {
-    /** Represents the valid state in a `TimeEntry`: having no `Project` selected.
-     Though this might be represented by a `nil` value, the state has associated strings and colors,
-     so I chose to design it as a specific state.
-     */
-    static func NoProject(childMOC moc: NSManagedObjectContext) -> Project {
-       Project(raw: RawProject(
-            id: NSNotFound,
-            is_private: false, /// inconsequential
-            wid: -1, /// placeholder value
-            hex_color: Constants.NoProjectHex,
-            name: "[No Project]",
-            billable: false, /// inconsequential
-            at: .distantPast
-       ), context: moc)
-    }
-    
-    static func AnyProject(childMOC moc: NSManagedObjectContext) -> Project {
-       Project(raw: RawProject(
-            id: NSNotFound,
-            is_private: false, /// inconsequential
-            wid: -1, /// placeholder value
-            hex_color: Constants.NoProjectHex,
-            name: "[Any Project]",
-            billable: false, /// inconsequential
-            at: .distantPast
-       ), context: moc)
-    }
-    
-    /** Represents a failure to identify some `Project`.
-     Usually this represents a state wherein a `Project` could not be found in our CoreData `Project` records,
-     or the records could not be accessed, preventing the `Project` from being identified.
-     */
-    static func UnknownProject(childMOC moc: NSManagedObjectContext) -> Project {
-       Project(raw: RawProject(
-            id: NSNotFound,
-            is_private: false, /// inconsequential
-            wid: -1, /// placeholder value
-            hex_color: Constants.NoProjectHex,
-            name: "[Unknown]",
-            billable: false, /// inconsequential
-            at: .distantPast
-       ), context: moc)
-    }
-}
-
 /// a collection of preset temporary `Project`s that can be created at will in any `NSManagedObjectContext`
 struct ProjectPresets {
+    
+    /// define a shared struct with a "floating" `NSManagedObjectContext`
+    static let shared = ProjectPresets(moc: NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType))
     
     let NoProject: Project
     let AnyProject: Project
     let UnknownProject: Project
     
-    init(childMOC moc: NSManagedObjectContext) {
-        NoProject = Project.NoProject(childMOC: moc)
-        AnyProject = Project.AnyProject(childMOC: moc)
-        UnknownProject = Project.UnknownProject(childMOC: moc)
+    init(moc: NSManagedObjectContext) {
+        NoProject = Project(raw: .NoProject, context: moc)
+        AnyProject = Project(raw: .AnyProject, context: moc)
+        UnknownProject = Project(raw: .UnknownProject, context: moc)
     }
 }
+
--- Data/RawStructs/RawProject.swift
+++ Data/RawStructs/RawProject.swift
@@ -25,3 +25,47 @@ struct RawProject: Decodable {
 //        var actual_hours: Int
 //        var color: Int // probably an enum for toggl's default color palette
 }
+
+//MARK:- RawProject Presets
+extension RawProject {
+    
+    /** Represents the valid state in a `TimeEntry`: having no `Project` selected.
+     Though this might be represented by a `nil` value, the state has associated strings and colors,
+     so I chose to design it as a specific state.
+     */
+    static let NoProject = RawProject(
+        id: NSNotFound,
+        is_private: false, /// inconsequential
+        wid: -1, /// placeholder value
+        hex_color: Constants.NoProjectHex,
+        name: "[No Project]",
+        billable: false, /// inconsequential
+        at: .distantPast
+    )
+    
+    /// Represents a state, mostly used in filters, that matches any project compared to it.
+    static let AnyProject = RawProject(
+        id: NSNotFound,
+        is_private: false, /// inconsequential
+        wid: -1, /// placeholder value
+        hex_color: Constants.NoProjectHex,
+        name: "[Any Project]",
+        billable: false, /// inconsequential
+        at: .distantPast
+   )
+    
+    
+    /** Represents a failure to identify some `Project`.
+     Usually this represents a state wherein a `Project` could not be found in our CoreData `Project` records,
+     or the records could not be accessed, preventing the `Project` from being identified.
+     */
+    static let UnknownProject = RawProject(
+        id: NSNotFound,
+        is_private: false, /// inconsequential
+        wid: -1, /// placeholder value
+        hex_color: Constants.NoProjectHex,
+        name: "[Unknown]",
+        billable: false, /// inconsequential
+        at: .distantPast
+    )
+}
