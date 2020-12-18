463466b2d70b8989ef1a38e2d72fc0ff5b28a946
Ipsky <yap@usc.edu>
2020-12-18 18:27:22 +0800

    move

--- Models/Project/ProjectLike.swift
+++ Models/Project/ProjectLike.swift
@@ -106,3 +106,26 @@ extension StaticProject {
         name: "[Unknown]"
     )
 }
+
+extension ProjectLike: Equatable {
+    static func < (lhs: ProjectLike, rhs: ProjectLike) -> Bool {
+        /// No Project should always be first
+        if ProjectLike.special(.NoProject) == lhs  { return true }
+        if ProjectLike.special(.NoProject) == rhs  { return false }
+        return lhs.name < rhs.name
+    }
+    
+    static func == (lhs: ProjectLike, rhs: ProjectLike) -> Bool {
+        switch (lhs, rhs) {
+        case (.project(let l), .project(let r)):
+            return l == r
+        /// since these  are known values, we can shortcut by comparing IDs
+        case (.special(let l), .special(let r)):
+            return l.id == r.id
+        case (.lite(let l), .lite(let r)):
+            return l == r
+        default:
+            return false
+        }
+    }
+}
