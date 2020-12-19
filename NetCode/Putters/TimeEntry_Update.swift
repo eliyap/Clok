7bbd356c3e11c4540c3964dd98e9a31aa30322cc
Ipsky <yap@usc.edu>
2020-12-19 14:50:25 +0800

    define upload task

--- NetCode/Putters/TimeEntry_Update.swift
+++ NetCode/Putters/TimeEntry_Update.swift
@@ -10,7 +10,22 @@ import Foundation
 import Combine
 
 extension RawTimeEntry {
-    func updateJob() {
+    
+    /// Creates an `uploadTask` to send changes to Toggl
+    /// - Parameter token: authentication token
+    func upload(with token: String) {
+        /// Docs @ https://github.com/toggl/toggl_api_docs/blob/master/chapters/time_entries.md#update-a-time-entry
+        var request = formRequest(
+            url: URL(string: "\(NetworkConstants.API_URL)/time_entries/\(id)")!,
+            auth: auth(token: token)
+        )
+        request.httpMethod = "PUT"
         
+        URLSession.shared.uploadTask(
+            with: request,
+            from: try! JSONEncoder().encode(self),
+            completionHandler: NetworkConstants.putHandler
+        )
+            .resume()
     }
 }
