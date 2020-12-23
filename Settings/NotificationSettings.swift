1d8fee1292ec79aa9fbf5ecc0228fd8a28bfd82a
Ipsky <yap@usc.edu>
2020-12-23 09:38:25 +0800

    add UN view

---
+++ Settings/NotificationSettings.swift
@@ -0,0 +1,19 @@
+//
+//  NotificationSettings.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 23/12/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+#warning("In Development")
+var NotificationSection: some View {
+    Section(header: Text("Notifications")) {
+        Button("Enable Notifications") {
+            
+        }
+    }
+}
