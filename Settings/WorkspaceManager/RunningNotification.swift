62f2c091b25979296dc530bba5b9589e20c57a80
Ipsky <yap@usc.edu>
2020-12-23 12:19:59 +0800

    add userdefault storage

---
+++ Settings/WorkspaceManager/RunningNotification.swift
@@ -0,0 +1,23 @@
+//
+//  RunningNotification.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 23/12/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import Foundation
+
+extension WorkspaceManager {
+    // MARK:- Running TimeEntry UserNotification UUID
+    private static let runningNotificationKey = "Clok.RunningNotification"
+    
+    static var RunningUUID: String? {
+        get {
+            return suite?.string(forKey: Self.runningNotificationKey)
+        }
+        set {
+            suite?.set(newValue, forKey: Self.runningNotificationKey)
+        }
+    }
+}
