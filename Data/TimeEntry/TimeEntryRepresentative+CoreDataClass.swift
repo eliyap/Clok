e32d2c85f0df0d592c7fc425078253670838214f
Ipsky <yap@usc.edu>
2020-12-24 17:01:08 +0800

    add representative

---
+++ Data/TimeEntry/TimeEntryRepresentative+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  TimeEntryRepresentative+CoreDataClass.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 24/12/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TimeEntryRepresentative)
+public class TimeEntryRepresentative: NSManagedObject {
+
+}
---
+++ Data/TimeEntry/TimeEntryRepresentative+CoreDataProperties.swift
@@ -0,0 +1,45 @@
+//
+//  TimeEntryRepresentative+CoreDataProperties.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 24/12/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension TimeEntryRepresentative {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<TimeEntryRepresentative> {
+        return NSFetchRequest<TimeEntryRepresentative>(entityName: "TimeEntryRepresentative")
+    }
+
+    @NSManaged public var name: NSObject?
+    @NSManaged public var project: Project?
+    @NSManaged public var represents: NSSet?
+
+}
+
+// MARK: Generated accessors for represents
+extension TimeEntryRepresentative {
+
+    @objc(addRepresentsObject:)
+    @NSManaged public func addToRepresents(_ value: TimeEntry)
+
+    @objc(removeRepresentsObject:)
+    @NSManaged public func removeFromRepresents(_ value: TimeEntry)
+
+    @objc(addRepresents:)
+    @NSManaged public func addToRepresents(_ values: NSSet)
+
+    @objc(removeRepresents:)
+    @NSManaged public func removeFromRepresents(_ values: NSSet)
+
+}
+
+extension TimeEntryRepresentative : Identifiable {
+
+}
