6c933ff0f66bb4ada6f49e0193465f58de02ec93
Ipsky <yap@usc.edu>
2020-12-24 16:50:01 +0800

    do work in background thread

--- Predictors/PrevNextIndexer.swift
+++ Predictors/PrevNextIndexer.swift
@@ -28,31 +28,38 @@ struct PrevNextIndexer {
     }
     
     static func prevNextIndex(context: NSManagedObjectContext) -> Void {
-        guard let target: TimeEntry = findTarget(context: context) else {
-            #if DEBUG
-            print("No Entries to index.")
-            #endif
-            return
+        DispatchQueue.global(qos: .background).async {
+            guard let target: TimeEntry = findTarget(context: context) else {
+                #if DEBUG
+                print("No Entries to index.")
+                #endif
+                return
+            }
+            let req = NSFetchRequest<NSFetchRequestResult>(entityName: TimeEntry.entityName)
+            /// to avoid hitting `CoreData` repeatedly, choose a range of dates to link together and fetch them at once
+            req.predicate = NSPredicate(format: "(start >= %@) AND (start <= %@)", NSDate(target.start), NSDate(target.start + .week))
+            req.sortDescriptors = [NSSortDescriptor(key: "start", ascending: true)]
+            guard let entries = try? context.fetch(req) as? [TimeEntry] else {
+                #if DEBUG
+                assert(false, "Failed to fetch entries to index!")
+                #endif
+                return
+            }
+            /// avoid in indexing error by aborting if there are too few entries
+            guard entries.count > 1 else { return }
+            (0...(entries.count - 2)).forEach{ entries[$0].next = entries[$0 + 1] }
+            entries.forEach{ $0.lastIndexed = Date() }
+            
+            /// move to main thread
+            DispatchQueue.main.async {
+                do { try context.save() }
+                catch { assert(false, "Failed to save after indexing!") }
+                #if DEBUG
+                print("Indexed \(entries.count) entries")
+                #endif
+            }
         }
-        let req = NSFetchRequest<NSFetchRequestResult>(entityName: TimeEntry.entityName)
-        /// to avoid hitting `CoreData` repeatedly, choose a range of dates to link together and fetch them at once
-        req.predicate = NSPredicate(format: "(start >= %@) AND (start <= %@)", NSDate(target.start), NSDate(target.start + .week))
-        req.sortDescriptors = [NSSortDescriptor(key: "start", ascending: true)]
-        guard let entries = try? context.fetch(req) as? [TimeEntry] else {
-            #if DEBUG
-            assert(false, "Failed to fetch entries to index!")
-            #endif
-            return
-        }
-        /// avoid in indexing error by aborting if there are too few entries
-        guard entries.count > 1 else { return }
-        (0...(entries.count - 2)).forEach{ entries[$0].next = entries[$0 + 1] }
-        entries.forEach{ $0.lastIndexed = Date() }
-        do { try context.save() }
-        catch { assert(false, "Failed to save after indexing!") }
-        #if DEBUG
-        print("Indexed \(entries.count) entries")
-        #endif
+        
     }
 
     /// only select entries which are potentially out of date due to having been updated recently
