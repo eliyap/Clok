03c257a43d087d21f94b6e473a7d6a7b170c4466
Ipsky <yap@usc.edu>
2020-12-26 17:45:43 +0800

    define new group

---
+++ Data/TimeEntry/TimeEntryRepresentative/CommonNext.swift
@@ -0,0 +1,13 @@
+//
+//  CommonNext.swift
+//  Clok
+//
+//  Created by Secret Asian Man Dev on 26/12/20.
+//  Copyright © 2020 Secret Asian Man 3. All rights reserved.
+//
+
+import Foundation
+
+extension TimeEntryRepresentative {
+    
+}
